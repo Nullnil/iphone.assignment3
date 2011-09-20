3569401f710589883dff45f701967e2b977aac2c
senHou <hsyy_1987@hotmail.com>
2011-09-20 23:34:37 +1000

    start up

---
+++ Assignment3/Assignment3/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Assignment3
+//
+//  Created by sen hou on 20/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
